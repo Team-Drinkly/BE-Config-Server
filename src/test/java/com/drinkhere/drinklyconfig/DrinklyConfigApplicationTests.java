12d8edf57621a58d2170d1b43968591378aa6d16
안정후 <austinan@naver.com>
2025-02-07 14:00:07 +0900

    chore: drinkly config server settings

---
+++ src/test/java/com/drinkhere/drinklyconfig/DrinklyConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkhere.drinklyconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinklyConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
